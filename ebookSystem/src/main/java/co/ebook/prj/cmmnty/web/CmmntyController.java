003d9fac6e6034b9e0590700e51940af4134d71d
DESKTOP-LAHJB9H\admin <chocopie4860@naver.com>
2021-10-18 16:52:59 +0900

    notice수정

---
+++ ebookSystem/src/main/java/co/ebook/prj/cmmnty/web/CmmntyController.java
@@ -0,0 +1,34 @@
+package co.ebook.prj.cmmnty.web;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import co.ebook.prj.cmmnty.service.CmmntyService;
+import co.ebook.prj.cmmnty.vo.CmmntyVO;
+
+@Controller
+public class CmmntyController {
+	@Autowired //2021.10.17 DAO연결하기
+	private CmmntyService cmmntyDao; 
+	
+	@RequestMapping("/noticeList")
+	String noticeList(Model model) {
+		CmmntyVO vo = new CmmntyVO();
+		vo.setCmmntyFlCd("01");
+		System.out.println("==========================>");
+		List<CmmntyVO> lists = cmmntyDao.cmmntyList(vo);
+		System.out.println( lists.size());
+		
+		for( CmmntyVO v : lists) {
+			System.out.println(v.toString());
+		}
+		
+		model.addAttribute("notices", lists);
+		return "notice/noticeList";
+	}		
+
+}
--- ebookSystem/src/main/webapp/WEB-INF/views/notice/noticeList.jsp
+++ ebookSystem/src/main/webapp/WEB-INF/views/notice/noticeList.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+    <%@ taglib uri="http://java.sun.com/jsp/jstl/core"  prefix="c"%>
+    
 <!DOCTYPE html>
 <html>
 <head>
@@ -8,5 +10,10 @@
 </head>
 <body>
 <h1>공지사항목록</h1>
+<div>
+<c:forEach var="notice" items="${notices }">
+		${notice.cmmntyId } : ${notice.cmmntyTitle }:${notice.cmmntyWriter }:${notice.cmmntyHit }<br>
+		</c:forEach>
+		</div>
 </body>
 </html>
